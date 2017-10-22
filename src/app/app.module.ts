c0119664a09df6a6255db429f238cced8b5d0de7
moran sharabi <moransh4@gmail.com>
2017-10-22 11:25:55 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { CubeComponent } from './cube/cube.component';
+import { SelectComponent } from './select/select.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CubeComponent,
+    SelectComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/typings.d.ts
@@ -0,0 +1,8 @@
+/* SystemJS module definition */
+declare var module: NodeModule;
+declare var classie:any;
+
+
+interface NodeModule {
+  id: string;
+}
