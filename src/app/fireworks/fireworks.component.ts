38f5f4860fe7cd0cbca5bd60eac5393e4ac8b70c
moran sharabi <moransh4@gmail.com>
2017-11-08 06:42:43 +0200

    add fireworks and rearrangement code

---
+++ src/app/fireworks/fireworks.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Injectable } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-fireworks',
+  templateUrl: './fireworks.component.html',
+  styleUrls: ['./fireworks.component.scss']
+})
+export class FireworksComponent implements OnInit {
+    constructor() { }
+
+  ngOnInit() {    
+  }
+
+}
+
