f957a4fd706b29f37a29bab6029e87d4fda293e9
moran sharabi <moransh4@gmail.com>
2018-06-09 15:48:44 +0300

    fix fireworks and update

--- src/app/fireworks/fireworks.component.ts
+++ src/app/fireworks/fireworks.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 
+declare const Fireworks;
 
 
 @Component({
@@ -8,9 +9,22 @@ import { Component, OnInit, Injectable } from '@angular/core';
   styleUrls: ['./fireworks.component.scss']
 })
 export class FireworksComponent implements OnInit {
-    constructor() { }
+  container : object ;
+  fireworks : object;
+  options : object = 
+  {
+      maxRockets: 10,            // max # of rockets to spawn
+      rocketSpawnInterval: 20, // millisends to check if new rockets should spawn
+      numParticles: 150,        // number of particles to spawn when rocket explodes (+0-10)
+      explosionHeight: 0.5,     // minimum percentage of height of container at which rockets explode
+      explosionChance: 0.08     // chance in each tick the rocket will explode
+  }
 
-  ngOnInit() {    
+  constructor() {}
+
+  ngOnInit() {   
+    this.container = document.getElementById('fireworks');
+    this.fireworks = new Fireworks.start(this.container, this.options)
   }
 
 }
